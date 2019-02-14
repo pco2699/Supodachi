871ea3b0053998ea4b48896f66f30cf815ddc131
pco2699 <aenkun@gmail.com>
2019-02-14 09:42:04 +0900

    検索BOXの追加

--- client/components/Map/SearchBox.vue
+++ client/components/Map/SearchBox.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="search-box">
-
-    <v-ons-input
-      class="input station"
-      placeholder="駅で検索"
-      modifier="underbar"
-      float
-    />
-    <v-ons-input
-      class="genre input"
-      placeholder="競技で検索"
-      modifier="underbar"
-      float
-    />
+    <v-ons-row class="search-item">
+      <v-ons-col>
+        <v-ons-icon class="icon" icon="fa-search" />
+        <v-ons-input
+          class="input station"
+          placeholder="駅で検索"
+          modifier="underbar"
+          float
+        />
+      </v-ons-col>
+    </v-ons-row>
+    <v-ons-row class="search-item">
+      <v-ons-col>
+        <v-ons-icon class="icon" icon="fa-futbol" />
+        <v-ons-input
+          class="genre input"
+          placeholder="競技で検索"
+          modifier="underbar"
+          float
+        />
+      </v-ons-col>
+    </v-ons-row>
+    <v-ons-row class="search-item">
+      <v-ons-col>
+        <v-ons-range v-model="volume" style="width: 35%;" />
+      </v-ons-col>
+    </v-ons-row>
   </div>
 </template>
 
@@ -27,11 +41,11 @@ export default {
   position: absolute;
   z-index: 1000;
 
-  width: 96%;
-  height: 200px;
+  width: 80%;
+  height: 150px;
 
-  left: 2%;
-  right: 2%;
+  left: 10%;
+  right: 10%;
   bottom: 30px;
 
   background-color: #ffffff;
@@ -39,9 +53,18 @@ export default {
   text-align: center;
 }
 
+.icon {
+  margin: 0 0 0 auto;
+  vertical-align: center;
+}
+
+.search-item {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
 .input {
-  margin: auto;
-  width: 240px;
-  display: block;
+  margin: 0 0 0 auto;
+  vertical-align: middle;
 }
 </style>
