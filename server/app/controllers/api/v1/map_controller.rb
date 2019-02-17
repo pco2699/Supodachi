312963ddb4137251787c24df2e860ccc232528f8
Ryujiro Sakai <sakateu@gmail.com>
2019-02-17 22:40:30 +0900

    施設と地名

--- server/app/controllers/api/v1/map_controller.rb
+++ server/app/controllers/api/v1/map_controller.rb
@@ -4,18 +4,25 @@ module Api
 
       # GET /api/v1/map
       def index
-        location = params[:lacation]
         event = params[:event]
         date = params[:date]
-        @facility = Facility.all
-        aaa = {location: location, event: event, date: date}
-        res = @facility
-        # render json: res
-        render json: aaa
+        if params[:location]
+          puts params[:location].class
+          location = Location.find_by_name(params[:location])
+          lat = location.lat
+          lon = location.lon
+          lat_five = 0.04506686487
+          lon_five = 0.05483202358
+          @facilities = Facility.where(
+              lat: (lat - lat_five)..(lat + lat_five),
+              lon: (lon - lon_five)..(lon + lon_five),
+              )
+        end
+        res = @facilities ? @facilities : {}
+        render json: res
       end
 
       def show
-
       end
     end
   end
