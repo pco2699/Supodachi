2473bf8babcf2f0444a78df1b584bf06248a3507
Ryujiro Sakai <sakateu@gmail.com>
2019-02-17 12:32:11 +0900

    frontとつなぎ込みようにテスト修正

--- server/app/controllers/api/v1/map_controller.rb
+++ server/app/controllers/api/v1/map_controller.rb
@@ -1,14 +1,17 @@
-require "#{Rails.root}/app/controllers/application_controller.rb"
-
 module Api
   module V1
     class MapController < ApplicationController
 
       # GET /api/v1/map
       def index
+        location = params[:lacation]
+        event = params[:event]
+        date = params[:date]
         @facility = Facility.all
+        aaa = {location: location, event: event, date: date}
         res = @facility
-        render json: res
+        # render json: res
+        render json: aaa
       end
 
       def show
